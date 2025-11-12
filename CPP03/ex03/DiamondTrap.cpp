cca18fbf75a9a062752c0053cc53bcf25da4d6f9
marouanelaamiri <malaamir@e2r6p13.1337.ma>
2025-11-12 18:00:34 +0100

    push

--- CPP03/ex03/DiamondTrap.cpp
+++ CPP03/ex03/DiamondTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: malaamir <malaamir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 11:20:01 by malaamir          #+#    #+#             */
-/*   Updated: 2025/11/11 17:51:21 by malaamir         ###   ########.fr       */
+/*   Updated: 2025/11/12 18:00:15 by malaamir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ DiamondTrap::DiamondTrap(void) : ClapTrap("Default_clap_name"), name("Default")
 {
 	std::cout << "DiamondTrap " << this->name << " created" << std::endl;
 	this->hitpoints = FragTrap::hitpoints;
-	this->energy_points = ScavTrap::energy_points;
 	this->attack_damage = FragTrap::attack_damage;
+	this->energy_points = 50;
 }
 
 DiamondTrap::DiamondTrap(std::string name)
@@ -26,7 +26,7 @@ DiamondTrap::DiamondTrap(std::string name)
 	std::cout << "DiamondTrap " << this->name << " created" << std::endl;
 	hitpoints = FragTrap::hitpoints;
 	attack_damage = FragTrap::attack_damage;
-	energy_points = 30; // ScavTrap energy points
+	energy_points = 50;
 }
 
 DiamondTrap::DiamondTrap(const DiamondTrap &other) : ClapTrap(other), ScavTrap(other), FragTrap(other)
