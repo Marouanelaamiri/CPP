07957abeecff6f86a959333cd39b9457310ec322
Marouane <maroaunelaamiri8@gmail.com>
2025-12-02 18:09:41 +0100

    push

--- CPP06/ex02/main.cpp
+++ CPP06/ex02/main.cpp
@@ -40,3 +40,66 @@ Base *generate(void)
 		return new class C();
 	}
 }
+void identify(Base* p)
+{
+    // Attempt 1: Is it A?
+    if (dynamic_cast<A*>(p) != NULL) 
+    {
+        std::cout << "A" << std::endl;
+    }
+    // Attempt 2: Is it B?
+    else if (dynamic_cast<B*>(p) != NULL)
+    {
+        std::cout << "B" << std::endl;
+    }
+    // Attempt 3: Is it C?
+    else if (dynamic_cast<C*>(p) != NULL)
+    {
+        std::cout << "C" << std::endl;
+    }
+}
+
+// How it works internally:
+
+// The program looks at the memory address p.
+
+// It follows the vptr to the internal type info.
+
+// It checks: "Does this type inherit from A?"
+
+// If Yes: Returns the address.
+
+// If No: Returns 0 (NULL).
+
+void identify(Base& p)
+{
+    // Probe 1: Force it to be A
+    try 
+    {
+        (void)dynamic_cast<A&>(p); // Cast to Reference
+        std::cout << "A" << std::endl;
+        return; // Success! We are done.
+    }
+    catch (std::exception &e) {} // It wasn't A. Swallow the error and continue.
+
+    // Probe 2: Force it to be B
+    try 
+    {
+        (void)dynamic_cast<B&>(p);
+        std::cout << "B" << std::endl;
+        return;
+    }
+    catch (std::exception &e) {}
+
+    // Probe 3: Force it to be C
+    try 
+    {
+        (void)dynamic_cast<C&>(p);
+        std::cout << "C" << std::endl;
+        return;
+    }
+    catch (std::exception &e) {}
+}
+
+// Why (void)? The compiler might warn you: "Value computed is not used." Casting to void silences this warning
+// because we don't care about the result variable; we only care if the line succeeded or threw an exception.
