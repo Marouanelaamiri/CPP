1aceec1f17f8b5ebde53e01beb59fdeb46e5a69b
marouanelaamiri <malaamir@e2r9p6.1337.ma>
2025-12-15 14:02:41 +0100

    cpp07

--- CPP07/README.md
+++ CPP07/README.md
@@ -9,3 +9,26 @@ a compile error happend , or at linker stage if we didnt implment the template o
 the header file (the compiler needs to se the template implemnetaion on hpp file, it
 need the entire source code of the template so it can copy-past and modify it accordinly)
 what problem they solve u ask , they solve DRY problem .
+
+
+what is "The Universal Caller" ?
+
+T_arr captures the exact type of the array (whether int or const int).
+
+T_func captures the exact type of the function pointer (whether it takes & or const &). The compiler handles the matching logic for you.
+
+If you pass a function pointer: void (*)(int&), type_func becomes that pointer type.
+
+If you pass a const function pointer: void (*)(const int&), type_func becomes that type.
+
+If you pass a Functor (a class with operator() defined), this works too!
+
+It handles Const Correctness automatically:
+
+If you pass int*, type_array is int.
+
+If you pass const int*, type_array becomes const int.
+
+The compiler automatically ensures you don't pass a const int* array to a function trying to modify it (it will generate a compilation error, which is the correct behavior).
+
+what is Complex Array Traversal ?
--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -6,104 +6,105 @@
 /*   By: malaamir <malaamir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/13 18:25:55 by malaamiri         #+#    #+#             */
-/*   Updated: 2025/12/15 13:34:00 by malaamir         ###   ########.fr       */
+/*   Updated: 2025/12/15 14:00:58 by malaamir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
-class Awesome
-{
-public:
-    Awesome(void) : _n(42) { return; } // Default value 42
-    int get(void) const { return this->_n; }
-private:
-    int _n;
-};
+// class Awesome
+// {
+// public:
+//     Awesome(void) : _n(42) { return; } // Default value 42
+//     int get(void) const { return this->_n; }
+// private:
+//     int _n;
+// };
 
-// Stream operator for Awesome
-std::ostream & operator<<(std::ostream & o, Awesome const & rhs) { 
-    o << rhs.get(); 
-    return o; 
-}
+// // Stream operator for Awesome
+// std::ostream & operator<<(std::ostream & o, Awesome const & rhs) { 
+//     o << rhs.get(); 
+//     return o; 
+// }
 
-// A GENERIC print function template
-template<typename T>
-void print(T const & x) { 
-    std::cout << x << std::endl; 
-    return; 
-}
+// // A GENERIC print function template
+// template<typename T>
+// void print(T const & x) { 
+//     std::cout << x << std::endl; 
+//     return; 
+// }
 
-int main() {
-    // 1. Test with primitive int array
-    int tab[] = { 0, 1, 2, 3, 4 }; 
-    // Call iter with the 'print' template instantiated for int
-    iter(tab, 5, print<int>);
+// int main() {
+//     // 1. Test with primitive int array
+//     int tab[] = { 0, 1, 2, 3, 4 }; 
+//     // Call iter with the 'print' template instantiated for int
+//     iter(tab, 5, print<int>);
     
-    // 2. Test with complex Awesome array
-    Awesome tab2[5]; // Creates 5 Awesome objects (all 42)
-    // Call iter with the 'print' template instantiated for Awesome
-    iter(tab2, 5, print<Awesome>);
+//     // 2. Test with complex Awesome array
+//     Awesome tab2[5]; // Creates 5 Awesome objects (all 42)
+//     // Call iter with the 'print' template instantiated for Awesome
+//     iter(tab2, 5, print<Awesome>);
 
-    return 0;
+//     return 0;
+// }
+
+// TEST FUNCTION 1: Takes Non-Const Reference (Modifies the value)
+void addOne(int &n)
+{
+	n++;
 }
 
-// // TEST FUNCTION 1: Takes Non-Const Reference (Modifies the value)
-// void addOne(int &n)
-// {
-// 	n++;
-// }
+// TEST FUNCTION 2: Takes Const Reference (Only reads the value)
+void printInt(const int &n)
+{
+	std::cout << n << " " ;
+}
+template <typename T_type>
+void printAny(const T_type &t)
+{
+	std::cout << t << " ";
+}
 
-// // TEST FUNCTION 2: Takes Const Reference (Only reads the value)
-// void printInt(const int &n)
-// {
-// 	std::cout << n << " " ;
-// }
-// void printString(const std::string &s)
-// {
-// 	std::cout << s << " ";
-// }
+int main()
+{
+	std::cout << "=== TEST 1: Int Array (Modification) ===" << std::endl;
+	int arr[] = {1, 2, 3, 4, 5};
 
-// int main()
-// {
-// 	std::cout << "=== TEST 1: Int Array (Modification) ===" << std::endl;
-// 	int arr[] = {1, 2, 3, 4, 5};
+	std::cout << "Original: ";
+	::iter(arr, 5, printInt); // Context: Mutable array, Read-only function
+	std::cout << std::endl;
 
-// 	std::cout << "Original: ";
-// 	::iter(arr, 5, printInt); // Context: Mutable array, Read-only function
-// 	std::cout << std::endl;
+	// Apply modification
+	::iter(arr, 5, addOne); // Context: Mutable array, Modifying function
 
-// 	// Apply modification
-// 	::iter(arr, 5, addOne); // Context: Mutable array, Modifying function
+	std::cout << "Modified: ";
+	::iter(arr, 5, printInt);
+	std::cout << std::endl;
 
-// 	std::cout << "Modified: ";
-// 	::iter(arr, 5, printInt);
-// 	std::cout << std::endl;
+	std::cout << "\n=== TEST 2: Const Int Array (Read-Only) ===" << std::endl;
+	const int c_arr[] = {10, 20, 30};
 
-// 	std::cout << "\n=== TEST 2: Const Int Array (Read-Only) ===" << std::endl;
-// 	const int c_arr[] = {10, 20, 30};
+	std::cout << "Values: ";
+	// Context: Const array, Read-only function
+	// If your template was wrong, this line would fail to compile.
+	::iter(c_arr, 3, printInt);
+	std::cout << std::endl;
 
-// 	std::cout << "Values: ";
-// 	// Context: Const array, Read-only function
-// 	// If your template was wrong, this line would fail to compile.
-// 	::iter(c_arr, 3, printInt);
-// 	std::cout << std::endl;
+	// this wont compile because c_arr is const
+	// ::iter(c_arr, 3, addOne);
+	// std::cout << std::endl;
 
-// 	// this wont compile because c_arr is const
-// 	// ::iter(c_arr, 3, addOne);
-// 	// std::cout << std::endl;
+	// std::cout << "Modified: ";
+	// ::iter(arr, 3, printInt);
+	// std::cout << std::endl;
 
-// 	// std::cout << "Modified: ";
-// 	// ::iter(arr, 3, printInt);
-// 	// std::cout << std::endl;
+	std::cout << "\n=== TEST 3: Const String Array (Read-Only) ===" << std::endl;
 
-// 	std::cout << "\n=== TEST 3: Const String Array (Read-Only) ===" << std::endl;
-
-// 	const std::string str_arr[] = {"Hello", "World", "!"};
-// 	std::cout << "Values: ";
-// 	::iter(str_arr, 3, printString);
-// 	std::cout << std::endl;
+	const std::string str_arr[] = {"Hello", "World", "!"};
+	std::cout << "Values: ";
+	::iter(str_arr, 3, printAny<std::string>);
+	std::cout << std::endl;
 	
 
-// 	return 0;
-// }
+	return 0;
+}
