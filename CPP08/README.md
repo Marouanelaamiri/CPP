87b580cdc0728d06b3345fe87452d34e570afc3a
marouanelaamiri <malaamir@e2r5p9.1337.ma>
2026-01-16 14:43:46 +0100

    cpp08

--- CPP08/README.md
+++ CPP08/README.md
@@ -41,6 +41,17 @@ begin(): Returns an iterator to the first element.
 
 end(): Returns an iterator to the theoretical element AFTER the last element. (Never dereference end()).
 
+Simplified Vector Iterator :
+template <typename T>
+struct VectorIterator {
+    T* _ptr; // The ONLY data member
+
+    // Overloading operators to fake pointer behavior
+    T& operator*() { return *_ptr; }           // Dereference
+    VectorIterator& operator++() { _ptr++; return *this; } // Increment
+    bool operator!=(const VectorIterator& rhs) { return _ptr != rhs._ptr; }
+};
+
 ### Algorithms
 
 Algorithms are function templates that perform operations on containers through iterators. They’re generic and work with any compatible container.
@@ -66,3 +77,36 @@ std::map<std::string, Customer> customerDb;
 std::set<int> uniqueIds;
 
 
+Method (obj.run()): used when the object is a "Noun" (Car, User, Bank) that has many behaviors.
+
+Functor (obj()): used when the object is a "Verb" (Comparator, Printer, Filter) wrapped in a class so it can store memory (state).
+
+Functors are simply Objects wearing a "Function Mask".
+
+By overloading operator(), you tell the compiler: "Treat this object as if it were a function."
+
+The Choice: Named Function vs. Functor
+You asked: "We can either create functions and call them on an object, or create a functor for that object?"
+
+What is a Functor?
+A Functor (or "Function Object") is simply a Class that behaves like a Function.
+
+You create it by defining a class and overloading the operator() (the parenthesis operator). This allows you to "call" the object just like you would call a normal function.
+
+
+
+"An iterator is an object that encapsulates a pointer to an element inside a container. It overloads operators like * (dereference) and ++ (increment) to provide a uniform interface for traversing different types of containers without exposing their underlying memory structure."
+
+The Hierarchy:
+
+Container: Holds the data (std::vector).
+
+Iterator: Points to the data (std::vector::iterator).
+
+Algorithm: Manipulates the data using the iterator (std::find).
+
+
+std::vector
+std::list
+std::map
+std::stack
--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: malaamir <malaamir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/23 18:05:03 by malaamir          #+#    #+#             */
-/*   Updated: 2025/12/26 19:28:50 by malaamir         ###   ########.fr       */
+/*   Updated: 2026/01/16 14:29:59 by malaamir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,79 @@
 #define EASYFIND_HPP
 
 #include <algorithm>
-#include <iterator>
-#include <exception>
 #include <iostream>
 #include <vector>
 #include <list>
-#include <deque>
 
-class NotFoundException : public std::exception
-{
-public:
-    virtual const char* what() const throw()
-    {
-        return "Element not found";
-    }
-};
 
+// Returns an iterator. 
+// If successful: returns iterator to value.
+// If failed: returns iterator to end().
 template <typename T>
 typename T::iterator easyfind(T& container, int value)
 {
-	typename T::iterator found = std::find(container.begin(), container.end(), value);
-	if (found == container.end())
-		throw NotFoundException();
-	return found;
+	return std::find(container.begin(), container.end(), value);
 }
 #endif
+
+// container.begin() pushes the memory address of the first integer onto the stack.
+
+// container.end() pushes the memory address of the "one-past-the-end" slot onto the stack.
+
+// std::find enters a loop.
+
+// It compares the value at the current address.
+
+// It increments the address by 4 bytes (if int).
+
+// If it hits the end address, it returns that end address to signal failure.
+
+
+
+
+// The Algorithm: std::find (Under the Hood)
+template<typename InputIterator, typename T>
+InputIterator find(InputIterator first, InputIterator last, const T& val)
+{
+    // The Loop
+    while (first != last) {
+        if (*first == val)  // Dereference and Compare
+            return first;   // Found it! Return the iterator (pointer)
+        ++first;            // Move pointer to next address (sizeof(T))
+    }
+    return last; // Returned end() because we hit the limit
+}
+
+// The Iterators: Simplified Vector Iterator
+template <typename T>
+struct VectorIterator {
+    T* _ptr; // The ONLY data member
+
+    // Overloading operators to fake pointer behavior
+    T& operator*() { return *_ptr; }           // Dereference
+    VectorIterator& operator++() { _ptr++; return *this; } // Increment
+    bool operator!=(const VectorIterator& rhs) { return _ptr != rhs._ptr; }
+};
+
+// Containers: std::vector (Under the Hood)
+template <typename T>
+class vector {
+    T* _start;          // Pointer to the first element
+    T* _finish;         // Pointer to the first EMPTY slot (size)
+    T* _end_of_storage; // Pointer to the end of allocated RAM (capacity)
+};
+
+// The hidden struct inside std::list
+template <typename T>
+struct Node {
+    T value;          // The actual data (e.g., int, string)
+    Node* next;       // Pointer to the NEXT node
+    Node* prev;       // Pointer to the PREVIOUS node
+};
+
+template <typename T>
+class list {
+    Node<T>* _head;   // Pointer to the first node
+    Node<T>* _tail;   // Pointer to the last node
+    size_t   _size;   // Tracks count (so .size() is fast)
+};
